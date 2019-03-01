cb45d1a918e9f0aa1ae72857cb4341719b279b1c
Ray Zane <raymondzane@gmail.com>
2019-03-01 11:53:24 -0500

    More comments

--- src/resources/Resource.ts
+++ src/resources/Resource.ts
@@ -35,30 +35,51 @@ export class Resource<
     this.api = api;
   }
 
+  /**
+   * List all objects
+   */
   public list(): Promise<T[]> {
     return getList(this.api.get());
   }
 
+  /**
+   * Get object by name
+   */
   public get(name: string): Promise<T> {
     return getBody(this.api(name).get());
   }
 
+  /**
+   * Create a new object
+   */
   public create(resource: T): Promise<T> {
     return getBody(this.api.post({ body: resource }));
   }
 
+  /**
+   * Replace an object by name
+   */
   public replace(name: string, resource: T): Promise<T> {
     return getBody(this.api(name).put({ body: resource }));
   }
 
+  /**
+   * Patch an object by name
+   */
   public patch(name: string, resource: DeepPartial<T>): Promise<T> {
     return getBody(this.api(name).patch({ body: resource }));
   }
 
+  /**
+   * Delete an object by name
+   */
   public delete(name: string): Promise<T> {
     return getBody(this.api(name).delete());
   }
 
+  /**
+   * Does an object with the given name exist?
+   */
   public async exists(name: string): Promise<boolean> {
     try {
       await this.api(name).get();
