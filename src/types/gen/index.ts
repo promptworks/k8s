a4b58d3b2a7321129b33543e3d7f46f37b02a97b
Ray Zane <raymondzane@gmail.com>
2019-03-08 21:35:13 -0500

    Generate more types!

--- src/types/gen/index.ts
+++ src/types/gen/index.ts
@@ -3,15 +3,18 @@
  */
 
 export * from "./configmap";
+export * from "./container";
 export * from "./cronjob";
 export * from "./daemonset";
 export * from "./deployment";
+export * from "./envvar";
 export * from "./horizontalpodautoscaler";
 export * from "./ingress";
 export * from "./job";
 export * from "./namespace";
 export * from "./persistentvolumeclaim";
 export * from "./pod";
+export * from "./probe";
 export * from "./replicaset";
 export * from "./secret";
 export * from "./service";
